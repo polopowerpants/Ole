cd050163ea2122b9c488640fa13a4fbc90980d01
polopowerpants Laptop <61581723+polopowerpants@users.noreply.github.com>
2020-09-21 17:19:50 +0200

    Lektion 1 + 2

--- ADA/cmake-build-debug/CMakeFiles/ADA.dir/DependInfo.cmake
+++ ADA/cmake-build-debug/CMakeFiles/ADA.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Kristian Larsen/Documents/GitHub/Ole/ADA/main.cpp" "C:/Users/Kristian Larsen/Documents/GitHub/Ole/ADA/cmake-build-debug/CMakeFiles/ADA.dir/main.cpp.obj"
+  "C:/Users/klars/Documents/GitHub/Ole/ADA/main.cpp" "C:/Users/klars/Documents/GitHub/Ole/ADA/cmake-build-debug/CMakeFiles/ADA.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
--- ADA/main.cpp
+++ ADA/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+using namespace std;
 
+// Lektion 1
+int sum(int n);
+int evenSquares(int n);
+int fib(int n);
+bool linear(string s, char c, int l);
+
+// Lektion 2
+#include "CircularQueue.cpp"
+#include "stack"
+bool balPar(string str);
+
+// Lektion 3
 #include "BallsnBins.cpp"
 #include <math.h>
 
-using namespace std;
+
 
 int main() {
 
-    int N = 100003;
+    int N = 10007;
     // random
     cout << "Random" << endl;
     cout << avr(1000, N, balls) << endl;
@@ -21,4 +34,68 @@ int main() {
 
 
     return 0;
+}
+
+
+
+// Lektion 2
+bool balPar(string str) {
+    // hvis denne skulle virke med { og [, så skulle der bare være flere if else.
+
+    stack<char> ole;
+
+    for (char c : str) {
+        if (c == '(') {
+            ole.push(c);
+        }
+        else if (c == ')') {
+            if (!ole.empty() && ole.top() == '(') {
+                ole.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return ole.empty();
+}
+
+
+// lektion 1
+bool linear(string s, char c, int l) {
+    if (l < 1) {
+        return false;
+    } else if (s[l-1] == c) {
+        return true;
+    } else {
+        return linear(s, c, l - 1);
+    }
+
+}
+int fib(int n) {
+
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+
+}
+int evenSquares(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    else {
+        return 4 * n * n + evenSquares(n-1);//pow(n * 2, 2) + evenSquares(n-1);
+    }
+}
+int sum(int n) {
+
+    if (n <= 0) {
+        return 0;
+    }
+    else {
+        return n + sum(n - 1);
+    }
 }
