a1a883df4fbc519cf5e7af58d9af05978df8ac8d
jacob <jacobkastrup@gmail.com>
2020-09-21 14:12:31 +0200

    Hejsa

---
+++ ADA/cmake-build-debug/CMakeFiles/ADA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ADA.exe"
+  "ADA.exe.manifest"
+  "ADA.lib"
+  "ADA.pdb"
+  "CMakeFiles/ADA.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ADA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
